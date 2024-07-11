bda64daed336b2cd7f20805fc08f98529653b443
MrChukwudi <chukwudidesire01@gmail.com>
2024-07-11 04:33:51 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+
+namespace WordUnscrambler
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			bool tryAgain = true;
+
+			while (tryAgain)
+			{
+				UnscrambleOperation unscrambleOperation = new UnscrambleOperation();
+
+
+				List<string> scrambledFile;
+				List<string> unScrambledFile;
+				List<string> resultString;
+				string scrambleFilePath;
+				string unScrambleFilePath;
+
+				Console.WriteLine("To select a choice, Chose between File or Manual");
+				string option = Console.ReadLine();
+
+				if (option == "File") //User choses to read from File
+				{
+					scrambleFilePath = Console.ReadLine();
+					Console.WriteLine("Enter File Path:");
+					scrambledFile = unscrambleOperation.ReadFile(scrambleFilePath);
+				}
+				else
+				{
+					scrambledFile = unscrambleOperation.ReadInput();
+				}
+
+				Console.WriteLine("Enter Unscramble File Path here:");
+				unScrambleFilePath = Console.ReadLine();
+				unScrambledFile = unscrambleOperation.ReadFile(unScrambleFilePath);
+
+				resultString = unscrambleOperation.UnscrambleWords(scrambledFile, unScrambledFile);
+
+
+				string formattedString = string.Join("\n\n", resultString);
+                Console.WriteLine(formattedString);
+
+                Console.WriteLine("Do you want to try again");
+				string myTryAgain = Console.ReadLine();
+				tryAgain = bool.Parse(myTryAgain);
+            }
+		}
+	}
+}
---
+++ UnscrambleOperation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WordUnscrambler
+{
+	public class UnscrambleOperation
+	{
+		
+		public List<string> ReadFile(string path)
+		{
+			List<string> readList = new List<string>();
+			string[] wordArray = File.ReadAllLines(path);
+			foreach(var word in wordArray)
+			{
+				readList.Add(word);
+			}
+			return readList;
+		}
+
+		public List<string> ReadInput()
+		{
+			Console.WriteLine("Enter the List of unscrambled words seperated by a comma");
+			string? unscrambleString = Console.ReadLine();
+
+			List<string> myList = new List<string>();
+
+			if (unscrambleString != null)
+			{
+				foreach (var str in unscrambleString.Split(","))
+				{
+					myList.Add(str.Trim());
+				}
+			}
+
+			return myList;
+		}
+
+
+		public List<string> UnscrambleWords(List<string> scrambled, List<string> unscrambled)
+		{
+			List<string> matched = new List<string>();
+
+			foreach (var scrambledWord in scrambled)
+			{
+				foreach (var unscrambledWord in unscrambled)
+				{
+					if (scrambledWord.Length == unscrambledWord.Length)
+					{
+						var sortedScrambled = String.Concat(scrambledWord.OrderBy(c => c)).ToLower();
+						var sortedUnscrambled = String.Concat(unscrambledWord.OrderBy(c => c)).ToLower();
+
+						if (sortedScrambled == sortedUnscrambled)
+						{
+							matched.Add($"{scrambledWord} matched {unscrambledWord}");
+						}
+					}
+				}
+			}
+
+			if(matched.Count == 0)
+			{
+				matched.Add("There was no match");
+			}
+
+			return matched;
+		}
+
+	}
+}
